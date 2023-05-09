dc7d1ce79c486635ba8707426e504d24a550eda3
Susie Gordon <susieyoungchoi@gmail.com>
2023-05-09 16:12:20 -0400

    updated answers inside Interact With the User & finished MadLib Story

--- mad_libs/app.js
+++ mad_libs/app.js
@@ -16,16 +16,21 @@ const words = {
 /* Start by creating a function called startMadlib that prompts a user to enter each word type and save their input 
 to the appropriate object key. */
 
-const startMadlib = ({number, adjective, pluralNoun, adverb, anotherAdjective}) => {
-    number = prompt("Give me any number");
-    adjective = prompt("Give me an adjective");
-    pluralNoun = prompt("Give me a plural noun");
-    adverb = prompt("Give me an adverb");
-    anotherAdjective = prompt("Give me another adjective");
-    return {number, adjective, pluralNoun, adverb, anotherAdjective}
+const startMadlib = () => {
+    words.number = prompt("Give me any number"); 
+    words.adjective = prompt("Give me an adjective");
+    words.pluralNoun = prompt("Give me a plural noun");
+    words.adverb = prompt("Give me an adverb");
+    words.anotherAdjective = prompt("Give me another adjective");
+    return {...words}
 
 }
-console.log(startMadlib(words));
+console.log(startMadlib());
 
+//////////// Create the Tech Startup MadLib Story ////////////
 
+const madlibStory = (words) => {
+    return alert(`Once upon a time a group of ${words.number} General Assembly graduates got together and made a startup called ${words.adjective} Technologies. Their goal was to create smart ${words.pluralNoun}.They approached the challenge ${words.adverb} which ultimately lead them to ${words.anotherAdjective} fame.`)
+}
+madlibStory(words);
 
