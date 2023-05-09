254470f5256b40d96e086209f3cd868e665e5be2
Susie Gordon <susieyoungchoi@gmail.com>
2023-05-09 08:17:00 -0400

    created a words object

---
+++ mad_libs/app.js
@@ -0,0 +1,22 @@
+//////////// Building an Object ////////////
+
+// First, create an object called words with the keys given below. Each key's property should be an empty string. This is where we will save the user's input later.
+// The keys: number, adjective, plural noun (pluralNoun), adverb, another adjective (anotherAdjective)
+
+const words = {
+    number: "",
+    adjective: "",
+    pluralNoun: "",
+    adverb: "",
+    anotherAdjective: "",
+}
+
+//////////// Interact With the User ////////////
+
+/* Start by creating a function called startMadlib that prompts a user to enter each word type and save their input to the appropriate object key. 
+For example, the following prompt's user input should be saved to the number key in our words object: */
+
+const startMadlib = (prompt) => {
+    
+}
+
